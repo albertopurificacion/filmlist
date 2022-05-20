84097eabb12f7e7c3b5f1f1c46eb6bce28237c21
Alberto Purificación Pérez <96584763+KataFurius@users.noreply.github.com>
2022-05-20 12:56:01 +0200

    Add files via upload

---
+++ files/index.php
@@ -0,0 +1,12 @@
+<?php 
+    include("resources/connection_api.php");
+    include("resources/templates/header.php");
+?>
+
+<div class="images-row mt-5">
+    <div class="row" id="colocar">
+     <?php include("resources/templates/card.php"); ?>
+    </div>
+</div>
+
+<?php include("resources/templates/footer.php") ;?>
