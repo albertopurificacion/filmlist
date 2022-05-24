547a333ceee9b31317eb370d91b8e20ee0feb5de
KataFurius <alberto.purificacion@educa.madrid.org>
2022-05-24 22:34:55 +0200

    d

--- files/resources/templates/header.php
+++ files/resources/templates/header.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TMDB</title>
+    <title>filmlist</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link href="public/css/style.css" rel="stylesheet">
     <link href="public/css/modal_card.css" rel="stylesheet">
